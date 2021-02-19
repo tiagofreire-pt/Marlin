059995e3d008c08a85e8be570c093fb29695fb99
Jyers <jyers.m@gmail.com>
2021-02-19 09:55:01 -0500

    getenv?

--- Configuration Files/E3V2 Templates/BLTouch-3x3/Configuration.h
+++ Configuration Files/E3V2 Templates/BLTouch-3x3/Configuration.h
@@ -129,7 +129,7 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD $BOARD
+  #define MOTHERBOARD getenv("BOARD")
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
